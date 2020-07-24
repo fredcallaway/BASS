2a9dde85a6a7f0b5a445e3a48bd04be103cbc89f
fredcallaway <fredc@llaway.com>
2020-07-24 15:52:44 -0700

    clean up

---
+++ utils.jl
@@ -0,0 +1,8 @@
+function mutate(x::T; kws...) where T
+    for field in keys(kws)
+        if !(field in fieldnames(T))
+            error("$(T.name) has no field $field")
+        end
+    end
+    return T([get(kws, fn, getfield(x, fn)) for fn in fieldnames(T)]...)
+end
