2c7b3c5f95873eab81a67f3ade830622174b6e51
fredcallaway <fredc@llaway.com>
2021-02-14 16:45:40 -0800

    more work on gp_likelihood

--- gp_likelihood.jl
+++ gp_likelihood.jl
@@ -1,4 +1,5 @@
 using GaussianProcesses
+using Distributions
 using Parameters
 using Serialization
 using SplitApplyCombine
@@ -6,6 +7,7 @@ using Statistics
 using Optim
 using Sobol
 using CSV
+using Strs
 
 include("utils.jl")
 include("model.jl")
@@ -15,144 +17,81 @@ include("likelihood.jl")
 include("ibs.jl")
 include("box.jl")
 include("figure.jl")
+include("minimize.jl")
+
+# %% ==================== Load results ====================
+
+struct SobolResult3
+    model::DataType
+    version::String
+    subject::Int
+    trials::Vector{HumanTrial}
+    ibs_kws::NamedTuple
+    box::Box
+    X::Matrix{Float64}
+    nll::Vector{Float64}
+    nll_std::Vector{Float64}
+end
 
 
-function fit_gp(xs, nll, nll_std; gp_mean = mean(nll))
-    X = reduce(hcat, xs)
+function SobolResult3(model, version, subject)
+    path = "tmp/$(lowercase(string(model)))/sobol/$version/$subject"
+    @unpack box, xs, results, chance, ibs_kws, trials, default = deserialize(path);
+
+    X = combinedims(xs)
+    nll = [-r.logp for r in results]
+    nll_std = [r.std for r in results] .|> fillmissing(20.)
+
+    SobolResult3(model, version, subject, trials, ibs_kws, box, X, nll, nll_std)
+end
+
+function true_nll(sr::SobolResult3, x; kws...)
+    m = sr.model(;sr.box(x)...)
+    -ibs_loglike(m, sr.trials[1:2:end]; sr.ibs_kws..., kws...).logp
+end
+
+function empirical_minimum(sr::SobolResult3; verbose=true)
+    i = argmin(sr.nll)
+    x = sr.X[:, i]
+    if verbose
+        println("Empirical minimum:")
+        println("  x              = ", round3.(x))
+        println("  observed nll   = ", round1(sr.nll[i]))
+        println("  recomputed nll = ", round1(true_nll(sr, x)))
+    end
+end
+
+# %% ==================== GP Surrogate ====================
+
+struct GPSL2 # gaussian process surrogate likelihood
+    sr::SobolResult3
+    gp::GPE
+end
+
+function fit_gp(X, nll, nll_std; gp_mean = mean(nll))
     gp_mean = MeanConst(gp_mean)
     kernel = SEArd(ones(size(X, 1)), -3.)
-
     log_noise = log.(nll_std)
-
-    train = eachindex(nll)[1:2:end]
-    test = eachindex(nll)[2:2:end]
-
-    # first fit on half the data to estimate out of sample prediction accuracy
-    gp = GPE(X[:, train], nll[train], gp_mean, kernel, log_noise[train])
-    optimize!(gp, domean=false, noise=false)
-    yhat, yvar = predict_f(gp, X[:, test])
-    rmse(preds) = √ mean((preds .- nll[test]).^2)
-    # mae(preds) = mean(abs.(preds .- nll[test]))
-    println("RMSE on held out data: ", rmse(yhat) |> round2)
-    println("RMSE of mean prediction: ", rmse(mean(nll[train])) |> round2)
-    println("RMSE of perfect prediction: ", √mean(nll_std) |> round2)
-    println("RMSE scaled by prediction variance: ", 
-        mean(((yhat .- nll[test]) ./ .√yvar).^2) |> round2)
-    
-    # use all the data for further analysis
     gp = GPE(X, nll, gp_mean, kernel, log_noise)
     optimize!(gp, domean=false, noise=false)
     gp
 end
 
-# %% --------
-
-function fillmissing2(target, repls)
-    @assert sum(ismissing.(target)) == length(repls)
-    repls = Iterators.Stateful(repls)
-    x = Array{Float64}(undef, length(target))
-    for i in eachindex(target)
-        if ismissing(target[i])
-            x[i] = first(repls)
-        else
-            x[i] = target[i]
-        end
+function cross_validate_gp(X, nll, nll_std; verbose=true, kws...)
+    train = eachindex(nll)[1:2:end]
+    test = eachindex(nll)[2:2:end]
+    gp = fit_gp(X[:, train], nll[train], nll_std[train]; kws...)
+    yhat, yvar = predict_f(gp, X[:, test])
+    rmse(preds) = √ mean((preds .- nll[test]).^2)
+    if verbose
+        println("RMSE on held out data: ", rmse(yhat) |> round2)
+        println("RMSE of mean prediction: ", rmse(mean(nll[train])) |> round2)
+        println("RMSE of perfect prediction: ", √mean(nll_std) |> round2)
+        println("RMSE scaled by prediction variance: ", 
+            mean(((yhat .- nll[test]) ./ .√yvar).^2) |> round2)
     end
-    x
+    return rmse(yhat)
 end
-
-function minimize(f::Function; restarts=20)
-    best_val = Inf; best_x = nothing
-    for x0 in Iterators.take(SobolSeq(n_free(box)), restarts)
-        res = optimize(x0) do x
-            f(x)
-        end
-        if res.minimum < best_val
-            best_x = res.minimizer
-            best_val = res.minimum
-        end
-    end
-    best_x, best_val
-end
-
-function minimize(f::Function, condition::Vector{Union{T,Missing}}; restarts=20) where T
-    best_val = Inf; best_x = nothing
-    for x0 in Iterators.take(SobolSeq(sum(ismissing.(condition))), restarts)
-        res = optimize(x0) do x_opt
-            x = fillmissing2(condition, x_opt)
-            f(x)
-        end
-        if res.minimum < best_val
-            best_x = res.minimizer
-            best_val = res.minimum
-        end
-    end
-    best_x, best_val
-end
-
-
-# %% ==================== Load results ====================
-run_name = "bddm/sobol/v7/"
-subjects = parse.(Int, readdir(runpath))
-subject = first(subjects)
-# path = path * subjects[1]
-@unpack box, xs, results, chance, ibs_kws, trials, default = deserialize("tmp/$run_name/$subject");
-
-nll = [-r.logp for r in results]
-nll_std = [r.std for r in results] .|> fillmissing(20.)
-
-function true_nll(x; kws...)
-    m = BDDM(;box(x)...)
-    -ibs_loglike(m, trials[1:2:end]; ibs_kws..., kws...).logp
-end
-
-
-# %% ==================== Empirical minimum ====================
-
-i = argmin(nll)
-emp_x, emp_y = xs[i], nll[i]
-
-@show emp_y
-@show true_nll(emp_x)
-
-# %% ==================== Plot "samples" ====================
-top = partialsortperm(nll, 1:100)
-samples = map(pairs(box.dims), xs[top] |> invert) do (name, d), x
-    name => x
-end |> Dict
-
-params = collect(keys(box.dims))
-
-# %% --------
-figure()
-    plot_grid(params, params) do vx, vy
-        scatter(samples[vx], samples[vy])
-        plot!(xlim=(0,1), ylim=(0,1), xlabel=string(vx), ylabel=string(vy))
-    end
-end
-
-# %% --------
-figure() do
-    vx = :base_precision
-    vy = :confidence_slope
-    scatter(samples[vx], samples[vy], smooth=true)
-    plot!(xlim=(0,1), ylim=(0,1), xlabel=string(vx), ylabel=string(vy))
-
-end
-
-# %% --------
-using DataFrames
-using RCall
-df = DataFrame(samples)
-
-# %% --------
-R"""
-round(cor($df), 2)
-"""
-
-# %% ==================== GP minimum ====================
-
-using Distributions
 
 # good = findall(nll_std .!= 20)
 order = sortperm(nll)
@@ -164,25 +103,30 @@ end
 to_fit = order[1:1000]
 gp = fit_gp(xs[to_fit], nll[to_fit], nll_std[to_fit]; gp_mean=-ibs_kws.min_multiplier * chance)
 
-predict_nll(x) = predict_f(gp, reshape(x, n_free(box), 1))[1][1]
 
-function predict_nll(x, quant)
-    y, yv = predict_f(gp, reshape(x, n_free(box), 1))
-    quantile(Normal(y[1], yv[1]), quant)
+function predict_meanstd(g::GPSL2, x)
+    f, fv = predict_f(g.gp, reshape(x, n_free(box), 1))
+    f[1], √fv[1]
 end
 
-function predict_nll_meanstd(x)
-    y, yv = predict_f(gp, reshape(x, n_free(box), 1))
-    y[1], √yv[1]
+function predict_quantile(g::GPSL2, x, q)
+    f, σ = predict_meanstd(x)
+    quantile(Normal(f, σ), q)
 end
 
-model_x, model_y = minimize(restarts=100) do x
-    predict_nll(x, 0.9)  # robustness -- look for minima with low variance
+function find_minimum(g::GPSL2)
+    model_x, model_y = minimize(restarts=100) do x
+        predict_quantile(g, x, 0.9)  # robustness -- look for minima with low variance
+    end
 end
 
-@show model_y
-true_model_y = @show true_nll(model_x)
+function estimate_error(g::GPLS2, x; q=0.5)
+    true_nll(g.sr, x) - predict_quantile(g, x, q)
+end
+
+true_model_y = true_nll(model_x)
 true_emp_y = @show true_nll(emp_x)
+@show model_y
 
 
 best_x = if true_model_y < true_emp_y
@@ -193,48 +137,18 @@ else
     emp_x
 end
 
-# %% --------
-m = BDDM(;box(best_x)...)
+# %% ====================  ====================
 
-mapmany(trials[1:end]) do t
-    map(1:1000) do i
-        sim = simulate(m, t)
-        pt1, pt2 = sim.presentation_times .* t.dt
-        val1, val2 = t.value
-        conf1, conf2 = t.confidence
-        m1, m2 = mean.(t.presentation_times)
-        order = m1 > m2 ? :longfirst : :shortfirst
-        (;t.subject, val1, val2, conf1, conf2, pt1, pt2, order, sim.choice)
-    end
-end |> CSV.write("results/v7-1064.csv")
-
-# %% --------
-map(1:10000) do i
-    t = SimTrial()
-    sim = simulate(m, t)
-    pt1, pt2 = sim.presentation_times .* t.dt
-    val1, val2 = t.value
-    conf1, conf2 = t.confidence
-    m1, m2 = mean.(t.presentation_times)
-    order = m1 > m2 ? :longfirst : :shortfirst
-    (;subject, val1, val2, conf1, conf2, pt1, pt2, order, sim.choice)
-end |> CSV.write("results/v7-1064-unyoked.csv")
+run_name = "bddm/sobol/v7/"
+subjects = parse.(Int, readdir(runpath))
+sr = @time SobolResult3(BDDM, "v7", 1064);
+empirical_minimum(sr)
 
 
 
 # %% ==================== Plot the marginals ====================
 
 function plot_marginals(xx, best_x, marginals)
-    ps = map(collect(pairs(box.dims)), enumerate(marginals)) do (name, d), (i, x)
-        maybelog = :log in d ? (:log,) : ()
-        plot(rescale(d, xx), x, xaxis=(string(name), maybelog...))
-        hline!([-chance, -chance*1.2], color=:gray, ls=:dash)
-        vline!([rescale(d, best_x[i])], c=:red)
-    end
-    plot(ps..., size=(600, 600))
-end
-
-function plot_marginals_meanstd(xx, best_x, marginals)
     ps = map(collect(pairs(box.dims)), enumerate(marginals)) do (name, d), (i, x)
         y, ystd = invert(x)
         maybelog = :log in d ? (:log,) : ()
@@ -258,7 +172,7 @@ end
 figpath = "figs/" * replace(replace(run_name, "sobol/" => ""), "/" => "-") * subject
 
 figure("$figpath-simple_marginal") do
-    plot_marginals_meanstd(xx, best_x, simple_marginals)
+    plot_marginals(xx, best_x, simple_marginals)
 end
 
 # %% --------
@@ -291,4 +205,39 @@ end
 #     end
 #     plot(ps..., size=(600, 600))
 # end
+
+
+# %% ==================== Simulate ====================
+
+
+m = BDDM(;box(best_x)...)
+
+
+mapmany(trials[1:end]) do t
+    map(1:1000) do i
+        sim = simulate(m, t)
+        pt1, pt2 = sim.presentation_times .* t.dt
+        val1, val2 = t.value
+        conf1, conf2 = t.confidence
+        m1, m2 = mean.(t.presentation_times)
+        order = m1 > m2 ? :longfirst : :shortfirst
+        (;t.subject, val1, val2, conf1, conf2, pt1, pt2, order, sim.choice)
+    end
+end |> CSV.write("results/v7-1064.csv")
+
+# %% --------
+
+function simulate(m; dt)
+    map(1:10000) do i
+        t = SimTrial()
+        sim = simulate(m, t)
+        pt1, pt2 = sim.presentation_times .* t.dt
+        val1, val2 = t.value
+        conf1, conf2 = t.confidence
+        m1, m2 = mean.(t.presentation_times)
+        order = m1 > m2 ? :longfirst : :shortfirst
+        (;subject, val1, val2, conf1, conf2, pt1, pt2, order, sim.choice)
+    end
+end
+simulate(m; trials[1].dt) |> CSV.write("results/v7-1064-unyoked.csv")
 
