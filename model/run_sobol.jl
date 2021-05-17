1fff324fcef02fb9f20a2ed0d7d634376c5add1a
fredcallaway <fredc@llaway.com>
2021-05-17 11:14:00 -0700

    sobol v12

--- model/run_sobol.jl
+++ model/run_sobol.jl
@@ -14,14 +14,22 @@ end
 include("sobol_search.jl")
 
 # %% --------
+# box = Box(
+#     base_precision = (.01, 2, :log),
+#     attention_factor = (0, 2),
+#     cost = (.01, .1, :log),
+#     confidence_slope = (0, 1),
+#     prior_mean = (-1, 1),
+# )
 box = Box(
-    base_precision = (.01, 2, :log),
-    attention_factor = (0, 2),
-    cost = (.01, .1, :log),
-    confidence_slope = (0, 1),
-    prior_mean = (-1, 1),
+    base_precision = (.01, .15, :log),
+    attention_factor = (0.5, 1),
+    cost = (.005, .015),
+    confidence_slope = (0, 0.05),
+    prior_mean = (-1, 0),
 )
 
 # run_sobol_group(BDDM, "test", box, 10, repeats=1, dt=.05, tol=20)
 # run_sobol_group(BDDM, "v11", box, 5000, repeats=10, dt=.025, tol=0)
-run_sobol_ind(BDDM, "v11", box, 5000, repeats=10, dt=.025, tol=0)
+# run_sobol_group(BDDM, "v12", box, 5000, repeats=10, dt=.025, tol=0)
+run_sobol_ind(BDDM, "v12", box, 5000, repeats=10, dt=.025, tol=0)
