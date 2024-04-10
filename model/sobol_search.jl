2f0c8d48d4b8c711886f7cbb5964f7e67a0ba203
fredcallaway <fredc@llaway.com>
2024-04-10 12:24:32 -0400

    sobol out

--- model/sobol_search.jl
+++ model/sobol_search.jl
@@ -1,21 +1,15 @@
-function sobol_search(box, N, data, out; dt=.025, ε=.05, tol=0, repeats=10, min_multiplier=1.2)
-
-    if isfile(out)
-        println("$out already exists")
-        return
-    end
-
+function sobol_search(box, N, data; dt=.025, ε=.05, tol=0, repeats=10, min_multiplier=1.2)
     xs = Iterators.take(SobolSeq(n_free(box)), N) |> collect
     trials = prepare_trials(Table(data); dt)
     ibs_kws = (;ε, tol, repeats, min_multiplier)
     models = map(xs) do x
         BDDM(;box(x)...)
     end
-    results = @showprogress "$out  " pmap(models) do m
+    results = @showprogress "sobol_search  " pmap(models) do m
         ibs_loglike(m, trials; ibs_kws...)
     end
     chance = chance_loglike(trials; ibs_kws.tol)
-    serialize(out, (;box, xs, models, trials, results, chance, ibs_kws, dt))
+    (;box, xs, models, trials, results, chance, ibs_kws, dt)
 end
 
 function run_sobol_ind(model, version, box, N; data=load_human_data(), kws...)
@@ -25,7 +19,9 @@ function run_sobol_ind(model, version, box, N; data=load_human_data(), kws...)
     @assert model == BDDM
     
     map(pairs(group(d->d.subject, data))) do (subj, subj_data)
-        sobol_search(box, N, subj_data, "$path/$subj"; kws...)
+        res = sobol_search(box, N, subj_data; kws...)
+        serialize("$path/$subj", res)
+        res
     end
 end
 
@@ -35,5 +31,7 @@ function run_sobol_group(model, version, box, N; data=load_human_data(), kws...)
 
     @assert model == BDDM
     
-    sobol_search(box, N, data, "$path/group"; kws...)
+    res = sobol_search(box, N, data; kws...)
+    serialize("$path/group", res)
+    res
 end
