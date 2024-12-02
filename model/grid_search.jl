9e804868a23161170ba164c15af3a0bc5a574417
fredcallaway <fredc@llaway.com>
2024-12-02 12:33:29 -0500

    recovery-artificial-rapid/2024-11-25

--- model/grid_search.jl
+++ model/grid_search.jl
@@ -1,27 +1,40 @@
 using ProgressMeter
 
+function flexmap(f, args...; progress=:default, parallel=false)
+    if progress == :default
+        progress = parallel && isa(stderr, Base.TTY)
+    end
+    mapfun = parallel ? pmap : map
+    if progress
+        progress_map(f, args...; mapfun)
+    else
+        mapfun(f, args...)
+    end
+end
+
 function grid_search(model, version, box, grid_size, data;
-    ε=0.01, tol=4, repeats=10, min_multiplier=1.2, progress=isa(stderr, Base.TTY))
+        ε=0.01, tol=4, repeats=10, min_multiplier=1.2, parallelize=:params
+    )
     path = "tmp/$(lowercase(string(model)))/grid/$version"
     println("Writing results to $path")
     mkpath(path)
-
-    candidates = map(grid(grid_size, box)) do g
-        model(; g...)
-    end
-
-    map(pairs(group(d -> d.subject, data))) do (subj, subj_data)
+    
+    flexmap(pairs(group(d -> d.subject, data)), parallel=parallelize == :subjects) do (subj, subj_data)
+        GC.gc()  # help out with memory
         out = "$path/$subj"
         if isfile(out)
             println("$out already exists")
             return
         end
 
-        println("Fitting subject $subj")
+        # println("Fitting subject $subj")
         trials = prepare_trials(Table(subj_data); dt=0.025)
+        candidates = map(grid(grid_size, box)) do g
+            model(; g...)
+        end
 
         ibs_kws = (; ε, tol, repeats, min_multiplier)
-        results = @showprogress out enabled=progress pmap(candidates) do m
+        results = flexmap(candidates, parallel=parallelize == :params) do m
             ibs_loglike(m, trials; ibs_kws...)
         end
         chance = chance_loglike(trials; ibs_kws.tol)
--- model/run_recovery_artificial.jl
+++ model/run_recovery_artificial.jl
@@ -2,34 +2,39 @@ using Distributed
 @everywhere begin
     include("base.jl")
     include("likelihood.jl")
+    include("grid_search.jl")
     using Serialization
 end
-include("grid_search.jl")
 using SplitApplyCombine
 using DataFrames, CSV
+using ProgressMeter
+ProgressMeter.ncalls(::typeof(flatmap), ::Any, xs::Any) = length(xs)
+
 # %% --------
 
-version = "2024-11-23"
-
-tmp_path = "tmp/bddm/grid/recovery-artificial/$version"
-results_path = "results/recovery-artificial/$version"
+version = "recovery-artificial-rapid/2024-11-25"
+tmp_path = "tmp/bddm/grid/$version"
+results_path = "results/$version"
 mkpath(tmp_path)
 mkpath(results_path)
 
 data1 = load_human_data(1)
 µ, σ = empirical_prior(data1)
+
 sim_box = fit_box = Box(
-    base_precision=.05,
-    attention_factor=(0.0, 1.5),
-    cost=.05,
+    base_precision=(0.01, 0.1),
+    attention_factor=(0., 1.5),
+    cost=(0.01, 0.1),
     prior_mean=µ,
     prior_precision=1 / σ^2,
 )
 
 SimTrial(t::SimTrial) = t
 
-# %% --------
-
+ALT_DURATIONS = Dict(
+    :shortfirst => [Normal(.025, 0), Normal(.05, 0)],
+    :longfirst => [Normal(.05, 0), Normal(.025, 0)]
+)
 function simulate_data(model; n_subjects=100, n_values=20)
     qs = range(0, 1, length=n_values+2)[2:end-1]
     vs = quantile.(Ref(Normal(µ, σ)), qs)
@@ -45,7 +50,7 @@ function simulate_data(model; n_subjects=100, n_values=20)
         HumanTrial(;
             value=[v1, v2],
             confidence=[4, 4],
-            presentation_distributions=PRESENTATION_DURATIONS[order],
+            presentation_distributions=ALT_DURATIONS[order],
             real_presentation_times=Int[],
             subject,
             choice=0,
@@ -64,17 +69,22 @@ function simulate_data(model; n_subjects=100, n_values=20)
     end
 end
 
-# %% --------
-
 if isempty(ARGS) || ARGS[1] == "setup"
-    params = reduce(vcat, grid(16, sim_box))
+    params = reduce(vcat, grid(7, sim_box))
+    filter!(p -> p.cost > 0.01, params)
     serialize("$tmp_path/params", params)
     @show length(params)
     println("Wrote $tmp_path/params")
 elseif ARGS[1] == "process"
     params = deserialize("$tmp_path/params")
 
-    flatmap(eachindex(params)) do param_id
+    param_ids = filter(eachindex(params)) do param_id
+        isdir("$tmp_path/$param_id")
+    end
+    if length(param_ids) < length(params)
+        println("Some params unavailable: $(setdiff(eachindex(params), param_ids))")
+    end
+    @showprogress flatmap(param_ids) do param_id
         flatmap(readdir("$tmp_path/$param_id")) do subject
             x = deserialize("$tmp_path/$param_id/$subject")
             map(x.candidates[:], x.results[:]) do c, r
@@ -92,7 +102,7 @@ else
     model = BDDM(; params[PARAM_ID]...)
     sim_data = simulate_data(model)
     @show length(sim_data)
-    grid_search(BDDM, "recovery-artificial/$version/$(PARAM_ID)", fit_box, 16, sim_data; repeats=10, ε=.05, tol=4)
+    grid_search(BDDM, "$version/$(PARAM_ID)", fit_box, 7, sim_data;
+        repeats=10, ε=.05, tol=4, parallelize=:params
+    )
 end
-
-
