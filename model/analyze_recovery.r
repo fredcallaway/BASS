e1482225edf323a140548f5239d7673412e1da6a
fredcallaway <fredc@llaway.com>
2024-12-04 10:42:58 -0500

    analyzing og recovery

--- model/analyze_recovery.r
+++ model/analyze_recovery.r
@@ -1,64 +1,48 @@
 # %% --------
 
-data <- read_csv("/tmp/data_longpres.csv") |> 
-    filter(val1 != val2) |> 
-    mutate(correct = (choice == 1) == (val1 > val2))
-
-# %% --------
-
-data |> 
-    group_by(attention_factor) |> 
-    summarize(mean(correct))
-
-data |> 
-    group_by(attention_factor) |> 
-    summarize(mean(is.na(initpresdur2)))
-
-data |> 
-    drop_na(initpresdur1, initpresdur2) |> 
-    ggplot(aes(val1 + val2, 1 * (choice == 1), color=factor(initpresdur1 > initpresdur2))) +
-    # stat_summary_bin(fun.data=mean_cl_normal, bins=10, geom="pointrange") +
-    gam_fit() +
-    facet_grid(~attention_factor)
-fig("attention_bias_shortpres",w=7)
-
-# %% --------
-
-data |> 
-    ggplot(aes(val1 - val2, 1 * (choice == 1))) +
-    gam_fit()
-fig(w=7)
-
-# %% --------
-data |> 
-    mutate(correct=(choice == 1) == (val1 > val2)) |> 
-    ggplot(aes(attention_factor, 1*correct)) +
-    points()
-fig(w=7)
-# %% --------
-
 source("base.r")
 
-# version <- "recovery/2024-11-20"
+version <- "recovery/2024-11-20"
+# version <- "recovery-artificial/2024-11-25B"
 # version <- "recovery-artificial/2024-11-23"
-version <- "recovery-artificial/2024-11-25B"
+# version <- "recovery-artificial-rapid/2024-11-25"
 
 generating <- read_csv(glue("results/{version}/generating_params.csv")) |> 
     select(!starts_with("prior"))
 
 likelihoods <- read_csv(glue("results/{version}/likelihoods.csv"))
 
-mle <- likelihoods |> 
+total_likelihoods <- likelihoods |> 
     group_by(param_id, cost, base_precision, attention_factor) |> 
+    filter(n() == max(n())) |> 
     summarize(
         logp = sum(logp),
         sd = sqrt(sum(std ^ 2))
-    ) |> 
+    )
+
+total_likelihoods |> filter(param_id == 27) |> 
+    drop_na(sd) |> 
+    with(sum(logp))
+
+likelihoods |> 
+    group_by(param_id, cost, base_precision, attention_factor) |> 
+    filter(n() == max(n())) |> 
+    ungroup() |> 
+    count(param_id) |> 
+    filter(param_id == 27)
+
+
+# generating |> 
+#     filter(between(attention_factor, .6, .7), base_precision == .07, cost==.04) |> 
+    
+
+mle <- total_likelihoods |> 
     group_by(param_id) |> 
     slice_max(logp) |> 
     left_join(generating, by="param_id", suffix=c("_fit", "_true"))
 
 # %% --------
+
 mle |>
     select(-c(logp, sd)) |> 
     pivot_longer(
@@ -71,6 +55,11 @@ mle |>
         names_from = type,
         values_from = value
     ) |> 
+    mutate(param = case_when(
+        param == "attention_factor" ~ "Attention Factor (θ)",
+        param == "cost" ~ "Sampling Cost (c)",
+        param == "base_precision" ~ "Baseline Precision (τ)"
+    )) |> 
     ggplot(aes(true, fit)) +
     geom_abline(slope=1, intercept=0) +
     geom_point(alpha=0.2) +
@@ -78,6 +67,118 @@ mle |>
     facet_wrap(~param, scales="free") +
     gam_fit(k=3, color=RED, fill=RED, alpha=0.1) +
     gridlines +
-    theme(aspect.ratio = 1)
+    theme(aspect.ratio = 1) +
+    labs(x="True Parameter Value", y="MLE Parameter Value")
 
-fig("recovery_artificial",w=7)
+fig("recovery",w=7)
+
+# %% --------
+
+total_likelihoods |> 
+    group_by(param_id, attention_factor) |> 
+    slice_max(logp) |> 
+    left_join(generating, by=c("param_id"), suffix=c("_fit", "_true")) |> 
+    filter(cost_true == .04, base_precision_true == .04) |> 
+    ggplot(aes(attention_factor_fit, logp)) +
+    geom_line() +
+    scale_color_brewer(palette="Set1") +
+    geom_vline(aes(xintercept=attention_factor_true), linetype="dashed") +
+    facet_wrap(~attention_factor_true, scales="free", nrow=2) +
+    theme(strip.text.x = element_blank())
+    # gam_fit(k=3, color=RED, fill=RED, alpha=0.1) +
+    # gridlines +
+    # theme(aspect.ratio = 1)
+
+fig("recovery-attention",w=10, h=5)
+
+# %% --------
+
+
+total_likelihoods |> 
+    group_by(param_id) |> 
+    left_join(generating, by="param_id", suffix=c("_fit", "_true")) |>
+    filter(cost_true == 0.04) |> 
+    group_by(param_id, base_precision_fit, attention_factor_fit) |> 
+    slice_max(logp) |> 
+    group_by(param_id) |> 
+    mutate(logp = logp - max(logp)) |> 
+    ggplot(aes(attention_factor_fit, base_precision_fit, fill=logp)) +
+    geom_tile() +
+    geom_point(aes(x=attention_factor_true, y=base_precision_true), 
+               shape='+', size=3, color='red') +
+    scale_fill_viridis_c() +
+    facet_grid(fct_rev(factor(base_precision_true)) ~ attention_factor_true, scales="free") +
+    labs(x="Fitted Attention Factor (θ)", 
+         y="Fitted Baseline Precision (τ)",
+         fill="Log Likelihood") +
+    theme(aspect.ratio = 1, 
+          strip.text.x = element_blank(), 
+          strip.text.y = element_blank())
+
+fig("recovery-heatmap", w=10, h=5)
+
+# %% --------
+total_likelihoods |> 
+    filter(param_id == 2) |> 
+    group_by(param_id) |> 
+    left_join(generating, by="param_id", suffix=c("_fit", "_true")) |>
+    group_by(param_id, abse_precision_fit, attention_factor_fit) |> 
+    slice_max(logp) |> 
+    group_by(param_id) |> 
+    mutate(logp = logp - max(logp)) |> 
+    ggplot(aes(attention_factor_fit, base_precision_fit, fill=logp)) +
+    geom_tile() +
+    geom_point(aes(x=attention_factor_true, y=base_precision_true), 
+               shape='+', size=3, color='red') +
+    scale_fill_viridis_c() +
+    facet_grid(fct_rev(factor(base_precision_true)) ~ attention_factor_true, scales="free") +
+    labs(x="Fitted Attention Factor (θ)", 
+         y="Fitted Baseline Precision (τ)",
+         fill="Log Likelihood") +
+    theme(aspect.ratio = 1, 
+          strip.text.x = element_blank(), 
+          strip.text.y = element_blank())
+
+fig("tmp", w=3, h=3)
+
+# %% --------
+
+total_likelihoods |> 
+    group_by(param_id) |> 
+    left_join(generating, by="param_id", suffix=c("_fit", "_true")) |>
+    filter(base_precision_true == 0.04) |> 
+    group_by(param_id, cost_fit, attention_factor_fit) |> 
+    slice_max(logp) |> 
+    group_by(param_id) |> 
+    mutate(logp = logp - max(logp)) |> 
+    ggplot(aes(attention_factor_fit, cost_fit, fill=logp)) +
+    geom_tile() +
+    geom_point(aes(x=attention_factor_true, y=cost_true), 
+               shape='+', size=3, color='red') +
+    scale_fill_viridis_c() +
+    facet_grid(fct_rev(factor(cost_true)) ~ attention_factor_true, scales="free") +
+    labs(x="Fitted Attention Factor (θ)", 
+         y="Fitted Sampling Cost (c)",
+         fill="Log Likelihood") +
+    theme(aspect.ratio = 1, 
+          strip.text.x = element_blank(), 
+          strip.text.y = element_blank())
+
+fig("recovery-heatmap-cost", w=10, h=5)
+
+# %% --------
+
+total_likelihoods |> 
+    group_by(param_id, attention_factor) |> 
+    slice_max(logp) |> 
+    left_join(generating, by=c("param_id"), suffix=c("_fit", "_true")) |> 
+    ggplot(aes(attention_factor_fit, logp, color=factor(attention_factor_true))) +
+    geom_point() +
+    geom_abline(slope=1, intercept=0) +
+    scale_color_brewer(palette="Set1") +
+    facet_grid(cost_true ~ base_precision_true)
+    # gam_fit(k=3, color=RED, fill=RED, alpha=0.1) +
+    # gridlines +
+    # theme(aspect.ratio = 1)
+
+fig("recovery-attention",w=7)
